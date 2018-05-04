6c85bc4f6ad825f77041999a7f7769d4d692216d
gulyasadam15 <gulyasadam15@gmail.com>
2018-05-04 03:49:56 -0700

    reducing code smells

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if(firingMode==FiringMode.SINGLE) {
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,11 +73,9 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
+    }
 
-      case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
+      else{
         if (! primaryTorpedoStore.isEmpty()) {
             firingSuccess = primaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = true;
@@ -87,9 +84,8 @@ public class GT4500 implements SpaceShip {
             firingSuccess = secondaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = false;
           }
-        
-        break;
-    }
+      }
+    
 
     return firingSuccess;
   }
